8a208fbf5f5b24b356bed30124e8bdff6b8b0440
Azarel Grahandito <skootydack@gmail.com>
2024-09-26 13:50:24 +0700

    feat: migrations for users and posts

--- database/migrations/2024_09_24_142943_create_posts_table.php
+++ database/migrations/2024_09_24_142943_create_posts_table.php
@@ -4,21 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->string('author');
-            $table->string('slug')->unique();
-            $table->text('content');
-            $table->timestamps();
-        });
+        Schema::create(
+            'posts',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('title');
+                $table->string('author');
+                $table->string('slug')->unique();
+                $table->text('content');
+                $table->timestamps();
+            }
+        );
     }
 
     /**
